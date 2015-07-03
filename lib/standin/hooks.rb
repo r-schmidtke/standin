7cb70f3cd3fa234767aca51bf03d87ba125f6291
Robin Schmidtke <robin@schmidtke-ct.de>
2015-07-03 12:19:38 +0200

    erased some old folders

--- lib/standin/hooks.rb
+++
@@ -1,7 +0,0 @@
-module StandIn
-  class Hooks < Redmine::Hook::ViewListener
-    
-    render_on :view_my_account_preferences,
-              :partial => 'issues/view_my_account_preferences'
-  end
-end
